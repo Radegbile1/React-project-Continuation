1fc945f0725cde4a6a498300f2b37bd3afa10b30
Radegbile1 <adegbilefunmilayoracheal@gmail.com>
2023-04-29 22:13:41 +0100

    update

--- people-data/src/App.js
+++ people-data/src/App.js
@@ -4,24 +4,29 @@ import list from './components/list';
 import lists, { data } from './components/list'
 function App() {
   
+  
   return (
     <div className="App">
-    
     <div>
       <input className='searchStyle' type="text" placeholder='search your name here...' />
     </div>
      <div className='searchResult'>
     {lists.map((list) => { 
+
+
+
       return (
+             
+
         <div  className='output'>
           <div>
           <img style= {{width: "20px", height:"20px"}} src={list.picture}/>
           </div>
           <div> 
-          {list.id}
+          key={list.id}
           <br></br> 
-           {list.firstName}
-          {list.lastName} 
+          key={list.firstName} 
+          key={list.lastName} 
           </div>
           
            </div>
